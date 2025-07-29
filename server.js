94b12344cf9e9e313bee0b119040a6303c4c8c93
ayahmeftah <ayahmeftah123123@gmail.com>
2025-07-29 11:03:07 +0300

    added imports, middlewares, port to server.js file

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require("express")
+const app = express()
+const dotenv = require("dotenv").config()
+const morgan = require("morgan")
+const methodOverride = require("method-override")
+const connectToDB = require("./config/db")
+
+
+// Middleware
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: false }));
+app.use(methodOverride("_method"));
+app.use(morgan("dev"))
+
+
+
+connectToDB()
+
+
+
+// Routes
+
+
+
+const port = process.env.PORT || 3000
+
+
+app.listen(port,()=>{
+    console.log("Listening on port " + port)
+})
