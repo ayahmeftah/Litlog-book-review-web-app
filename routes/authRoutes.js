4d7cfd76d6b1f83cf124c664256949c615c31b7c
ayahmeftah <ayahmeftah123123@gmail.com>
2025-07-29 14:20:39 +0300

    created sign up page ejs with get route

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -2,7 +2,9 @@ const User = require("../models/User")
 const router = require("express").Router()
 const bcrypt = require("bcrypt")
 
-
+router.get('/sign-up', async (req,res)=>{
+    res.render("auth/sign-up.ejs")
+})
 
 
 
