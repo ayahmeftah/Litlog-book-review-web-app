a55716e64b7bf2e39cc0386f74b0ff28efb8e343
ayahmeftah <ayahmeftah123123@gmail.com>
2025-07-29 10:59:47 +0300

    created db connection

---
+++ config/db.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose")
+async function connectToDB(){
+    try{
+        await mongoose.connect(process.env.MONGODB_URI)
+        console.log("Connected to Database")
+    }
+    catch(error){
+        console.log("Error Occured",error)
+    }
+}
+
+module.exports = connectToDB
