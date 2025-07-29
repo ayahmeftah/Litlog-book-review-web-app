fe9837217bf0ca6dc78262e07b4554b3c31c5793
ayahmeftah <ayahmeftah123123@gmail.com>
2025-07-29 12:25:20 +0300

    imported cloudinary in server.js and tested connection

--- config/cloudinary.js
+++ config/cloudinary.js
@@ -1,13 +1,9 @@
 const cloudinary = require('cloudinary').v2;
 
-function connectToCloudinary() {
+cloudinary.config({
+    cloud_name: process.env.CLOUD_NAME,
+    api_key: process.env.API_KEY,
+    api_secret: process.env.API_SECRET
+});
 
-    cloudinary.config({
-        cloud_name: process.env.CLOUD_NAME,
-        api_key: process.env.API_KEY,
-        api_secret: process.env.API_SECRET
-    });
-
-}
-
-module.exports = connectToCloudinary
+module.exports = cloudinary
--- server.js
+++ server.js
@@ -4,10 +4,9 @@ const dotenv = require("dotenv").config()
 const morgan = require("morgan")
 const methodOverride = require("method-override")
 const connectToDB = require("./config/db")
-const connectToCloudinary = require("./config/cloudinary")
-const mutler = require("multer")
+const cloudinary = require("./config/cloudinary")
 
-
+console.log("Cloudinary ready:", cloudinary.config().cloud_name);
 
 // Middleware
 app.use(express.static('public'));
@@ -18,7 +17,6 @@ app.use(morgan("dev"))
 
 
 connectToDB()
-// connectToCloudinary()
 
 
 // Routes
