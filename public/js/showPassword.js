0e3f7f456d9947f9236474ffe45e5d33817645b6
ayahmeftah <ayahmeftah123123@gmail.com>
2025-07-30 05:41:23 +0300

    created show password button functionality with dom

---
+++ public/js/showPassword.js
@@ -0,0 +1,20 @@
+function init() {
+  const passwordInputElem = document.querySelector("#password");
+  const showBtnElem = document.querySelector("#show-password");
+
+  if (passwordInputElem && showBtnElem) {
+    showBtnElem.addEventListener("click", function (e) {
+      e.preventDefault();
+
+      if (passwordInputElem.type === "password") {
+        passwordInputElem.type = "text"
+        showBtnElem.textContent = "Hide"
+      } else {
+        passwordInputElem.type = "password"
+        showBtnElem.textContent = "Show"
+      }
+    })
+  }
+}
+
+document.addEventListener("DOMContentLoaded", init);
