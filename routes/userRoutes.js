458fb242b695ee17ee39558bfe46df6c3545f368
ayahmeftah <ayahmeftah123123@gmail.com>
2025-08-01 19:44:14 +0300

    added get for edit profile page with ejs

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -5,10 +5,27 @@ const setupMulter = require("../middleware/multer")
 const uploadBookCover = setupMulter("profile-pictures")
 
 router.get("/profile", requireLogin, async (req, res) => {
-  const user = await User.findById(req.session.user._id)
-  const totalBooks = user.bookList.length
+    try {
+        const user = await User.findById(req.session.user._id)
+        const totalBooks = user.bookList.length
 
-  res.render("users/profile.ejs", {user,totalBooks})
+        res.render("users/profile.ejs", { user, totalBooks })
+
+    } catch (error) {
+        console.log("Get profile error:", error)
+        res.redirect("/books")
+    }
+})
+
+router.get("/profile/edit", requireLogin, async (req, res) => {
+    try {
+        const user = await User.findById(req.session.user._id)
+        res.render("users/edit-profile.ejs", { user, error: null })
+
+    } catch (error) {
+        console.log("Get edit profile error:", error)
+        res.redirect("/users")
+    }
 })
 
 
