be5c8e3ed92f35c470f5a2e21b775a646a35acf6
ayahmeftah <ayahmeftah123123@gmail.com>
2025-08-03 23:07:41 +0300

    completed edif profile form style and changed show password from id to class

--- public/js/showPassword.js
+++ public/js/showPassword.js
@@ -1,6 +1,6 @@
 function init() {
-  const passwordInputElem = document.querySelector("#password");
-  const showBtnElem = document.querySelector("#show-password");
+  const passwordInputElem = document.querySelector(".password");
+  const showBtnElem = document.querySelector(".show-password");
 
   if (passwordInputElem && showBtnElem) {
     showBtnElem.addEventListener("click", function (e) {
